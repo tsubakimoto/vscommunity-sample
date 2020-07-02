069655e6431544de1d3e128fbeed9699c88a462d
tsubakimoto <tkmbmbr@gmail.com>
2020-07-03 00:02:16 +0900

    Add simple console application

---
+++ VsCommuunitySample.SimpleConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VsCommuunitySample.SimpleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
