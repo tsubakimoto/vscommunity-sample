d3c4f3e4dbadb9f7f60758d742e59ea6aecf3ec7
tsubakimoto <tkmbmbr@gmail.com>
2020-07-03 00:03:18 +0900

    Add simple generic host application

---
+++ VsCommuunitySample.SimpleGenericHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VsCommuunitySample.SimpleGenericHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
